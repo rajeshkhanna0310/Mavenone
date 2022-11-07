5372acd33b96ed4d7063a1eb386f7928bccfd797
rajeshkhanna0310 <rrajeshkhanvel@gmail.com>
2022-11-07 23:48:29 +0530

    f

---
+++ src/test/java/com/gittest/GitOne.java
@@ -0,0 +1,17 @@
+package com.gittest;
+
+public class GitOne {
+	public void method1() {
+		System.out.println("one");
+
+	}
+	public void method2() {
+		System.out.println("one");
+		
+	}
+	public void method3() {
+		System.out.println("one");
+		
+	}
+
+}
